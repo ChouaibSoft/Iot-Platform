336ab45572b07ba7c9acc01756acb100d801567e
Chouaib LOKBANI <clokbani@chouaib.fedora>
2019-04-14 20:39:04 +0100

    Aside-menu transition efect

--- src/components/AsideMenu.vue
+++ src/components/AsideMenu.vue
@@ -5,33 +5,69 @@
                 <li class="small-cap">
                     <span>Personnal</span>
                 </li>
-                <li class="item-header active">
-                    <a href="#" class="group-name has-arrow">
+                <li class="item-header" v-bind:class="{active: selected === 'item1'}">
+                    <a  class="group-name has-arrow" @click="selected = 'item1'">
                         <i class="fa fa-home"></i>
                         <span>Dashboard</span>
                     </a>
-                    <div class="items-group">
-                        <ul>
-                            <li>
-                                <a href="#">
-                                    <i class="fa fa-dot-circle"></i>
-                                    <span>Dashboard-1</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <i class="fa fa-dot-circle"></i>
-                                    <span>Dashboard-2</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <i class="fa fa-dot-circle"></i>
-                                    <span>Dashboard-3</span>
-                                </a>
-                            </li>
-                        </ul>
+                    <transition name="slide">
+                        <div v-if="selected === 'item1'">
+                            <ul class="items-group">
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-dot-circle"></i>
+                                        <span>Dashboard-1</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-dot-circle"></i>
+                                        <span>Dashboard-2</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-dot-circle"></i>
+                                        <span>Dashboard-3</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </transition>
+                </li>
+
+                <li class="small-cap">
+                    <span>Information</span>
+                </li>
+                <li class="item-header" v-bind:class="{active: selected === 'item2'}">
+                    <a  class="group-name has-arrow" @click="selected = 'item2'">
+                        <i class="fa fa-home"></i>
+                        <span>Dashboard</span>
+                    </a>
+                    <transition name="slide">
+                        <div v-if="selected === 'item2'">
+                            <ul class="items-group">
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-dot-circle"></i>
+                                        <span>Dashboard-1</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-dot-circle"></i>
+                                        <span>Dashboard-2</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-dot-circle"></i>
+                                        <span>Dashboard-3</span>
+                                    </a>
+                                </li>
+                            </ul>
                     </div>
+                    </transition>
                 </li>
                 <li class="small-cap">
                     <span>Information</span>
@@ -75,6 +111,11 @@
     import {  mapActions } from 'vuex';
     export default {
         name: "AsideMenu",
+        data(){
+            return{
+                selected: 'item1'
+            }
+        },
         methods: {
             ...mapActions([
                 'logout'
@@ -84,5 +125,35 @@
 </script>
 
 <style scoped>
+    .slide-enter-active {
+        -moz-transition-duration: 0.3s;
+        -webkit-transition-duration: 0.3s;
+        -o-transition-duration: 0.3s;
+        transition-duration: 0.3s;
+        -moz-transition-timing-function: ease-in;
+        -webkit-transition-timing-function: ease-in;
+        -o-transition-timing-function: ease-in;
+        transition-timing-function: ease-in;
+    }
 
+    .slide-leave-active {
+        -moz-transition-duration: 0.3s;
+        -webkit-transition-duration: 0.3s;
+        -o-transition-duration: 0.3s;
+        transition-duration: 0.3s;
+        -moz-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+        -webkit-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+        -o-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+        transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+    }
+
+    .slide-enter-to, .slide-leave {
+        max-height: 100px;
+        overflow: hidden;
+    }
+
+    .slide-enter, .slide-leave-to {
+        overflow: hidden;
+        max-height: 0;
+    }
 </style>
